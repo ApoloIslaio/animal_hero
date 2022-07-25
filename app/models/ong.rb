5e1be84a945ba0c158e258453c4edbf636617cac
Apolo Islaio <apoloislaio@gmail.com>
2022-07-25 11:54:02 -0300

    validar o formulário de criação de ong

--- app/models/ong.rb
+++ app/models/ong.rb
@@ -1,2 +1,17 @@
 class Ong < ApplicationRecord
+  validates :nome, presence: true, length: { maximum: 50 }
+
+  before_save :email_downcase
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: true }
+  validates :estado, presence: true, length: { maximum: 50 }
+
+  private
+  def email_downcase
+    self.email.downcase!
+  end
 end
