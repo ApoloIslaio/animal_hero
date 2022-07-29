a9305d0d3d2a1104451ab5a6766786b22bdaacf9
Apolo Islaio <apoloislaio@gmail.com>
2022-07-29 09:06:25 -0300

    estilização da tabela clinics

--- app/helpers/clinics_helper.rb
+++ app/helpers/clinics_helper.rb
@@ -1,2 +1,11 @@
 module ClinicsHelper
+  def renderImg(src)
+
+    if src != ''
+      image_tag src, size: "60x65" 
+    else 
+      'Não informado.' 
+    end 
+
+  end
 end
--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,3 +1,18 @@
 class Clinic < ApplicationRecord
   belongs_to :state
+
+  validates :description, presence: true, length: { maximum: 50 }
+
+  before_save :email_downcase
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: true }
+
+  private
+  def email_downcase
+    self.email.downcase!
+  end
 end
